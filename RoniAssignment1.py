04c809919179dc064ce0d3195417b3082818eb3e
Roni Kulkarni <ronivkulkarni@gmail.com>
2025-05-07 22:27:12 -0400

    Assignment 1 Submission

---
+++ RoniAssignment1.py
@@ -0,0 +1,25 @@
+# Task 1: Variable testing
+name = "Roni"
+age = 22
+height = 6.0
+print("Hey there, my name is ",name,"! I’m ",age," years old and ",height," feet tall.")
+
+# Task 2: Operators-Playing with Numbers
+
+num1 = 45
+num2 = 94
+print("The sum of ",num1," and ",num2," is: ", num1 + num2)
+print("The difference of ",num2," and ",num1," is: ", num2 - num1)
+print("The product of ",num1," and ",num2," is: ", num1 * num2)
+print("The quotient of ",num2," and ",num1," is: ", num2 / num1)
+
+# Task 3: Conditional statements
+
+num = int(input("Please enter a number "))
+
+if num > 0:
+    print("This number is positive. Awesome!")
+elif num < 0:
+    print("This number is negative. Better luck next time!")
+else:
+    print("Zero it is. A perfect balance!")
