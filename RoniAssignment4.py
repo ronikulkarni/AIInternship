f063dfcf79bf007cf1e704af4349c930037cd1cc
ronikulkarni <ronivkulkarni@gmail.com>
2025-05-22 23:06:56 -0400

    submitting assignment 4

---
+++ RoniAssignment4.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------
+#    Author: Roni Kulkarni
+#    Date : 05-22-2025
+#    Number Guessing Game
+
+#-------------------------------------------------
+
+
+import random
+randomNum = random.randint(1, 100)
+
+userNum = 0
+numAttempts = 0
+
+while(userNum != randomNum):
+    userNum = int(input("Please enter a number between 1 and 100 "))
+    numAttempts = numAttempts + 1
+    if (userNum > randomNum):
+        print("Too high! Try again.")
+    elif (userNum < randomNum):
+        print("Too low! Try again.")
+    else:
+        print("Congratulations! You guessed it in" , numAttempts, "attempts!")
+        break
+    
