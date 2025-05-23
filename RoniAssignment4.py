72dc2344c313cca1d65c64e7382932ba5a499011
ronikulkarni <ronivkulkarni@gmail.com>
2025-05-22 23:32:30 -0400

    added bonus task

--- RoniAssignment4.py
+++ RoniAssignment4.py
@@ -15,6 +15,9 @@ numAttempts = 0
 while(userNum != randomNum):
     userNum = int(input("Please enter a number between 1 and 100 "))
     numAttempts = numAttempts + 1
+    if (numAttempts == 10):
+        print("Game over! Better luck next time!")
+        break
     if (userNum > randomNum):
         print("Too high! Try again.")
     elif (userNum < randomNum):
